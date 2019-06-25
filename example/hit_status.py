d5ebced16aceba246cb43d96ee32017e6a813df6
Yanzhao Wang <wyanzhao1996@gmail.com>
2019-06-24 19:48:34 -0700

    1.switched serilization format from json to pickle 2.implementated query cache status according to commit address

--- example/hit_status.py
+++ example/hit_status.py
@@ -10,21 +10,50 @@ sys.path.append(".")
 from verilog_vcd import verilog_vcd
 import bitarray
 import json
+import pickle
 
 number_sets = 64
 number_ways = 4
 
+DEBUG_MODE = True
+
+def find_cache_hit(meta_idx, tag, cache_trace_status, is_boom_orignal: bool):
+    hit_status = []
+    if is_boom_orignal:
+
+        #TODO: Implementate boom origin cache dump function.
+
+        return
+    else:
+        for i in cache_trace_status:
+            tsc = i[0]
+            cache_status = i[1]
+            commit_addr = i[2]
+            hit_set = cache_status[str(meta_idx)]
+            s1_dc_bypass = True if cache_status.get('s1_dc_bypass') == '1' else False
+            s1_load_retry = True if cache_status.get('s1_load_retry') == '1' else False
+            for way in hit_set:
+                s_valid = True if hit_set.get(way).get('s') == '1' else False
+                if tag == hit_set.get(way).get('tag[19:0]') and (not s_valid or (s1_dc_bypass and s1_load_retry)):
+                    hit_status.append("commit inst: {} tsc:{} hit set:{} way:{}".format(hex(commit_addr), tsc, meta_idx, way))
+        return hit_status
+
 def main():
-    trace_status = {}
-
-    if len(sys.argv) != 3:
-        raise Exception("give inputs like python3 hit_status.py 0x800000 trace-dump.json")
+    cache_trace = {}
+    if not DEBUG_MODE:
+        if len(sys.argv) != 3:
+            raise Exception("give inputs like python3 hit_status.py 0x800000 trace-dump.pickle")
     
-    with open(sys.argv[2], "r") as f:
-        trace_status = json.load(f)
+        with open(sys.argv[2], "rb") as f:
+            cache_trace = pickle.load(f)
+    else:
+        with open("/home/wyanzhao/Workspace/python-vcd/dump-vcd.pickle", "rb") as f:
+            cache_trace = pickle.load(f)
 
-    inst_addr = sys.argv[1]
-    #inst_addr = "80002678"
+    if not DEBUG_MODE:
+        inst_addr = sys.argv[1]
+    else:
+        inst_addr = "80002678"
     inst_addr = int(inst_addr, 16)
     
     if inst_addr.bit_length() != 32:
@@ -35,15 +64,12 @@ def main():
 
     meta_idx = int(meta_idx.to01(), 2)
 
-    hit_status = []
-    for i in trace_status:
-        tsc = i[0]
-        cache_status = i[1]
-        hit_set = cache_status[str(meta_idx)]
-        for way in hit_set:
-            if tag == hit_set.get(way):
-                hit_status.append("tsc: {} ways: {}".format(tsc, way))
-    print(hit_status)
+    hit_status = find_cache_hit(meta_idx, tag, cache_trace, False)
+    with open("hit_status.txt", "w") as f:
+        for i in hit_status:
+            f.write(i + "'\n")
+
+    return 0
 
 if __name__ == "__main__":
     main()
--- example/vcd_analyze.py
+++ example/vcd_analyze.py
@@ -7,6 +7,7 @@ sys.path.append(".")
 
 import verilog_vcd
 import json
+import pickle
 
 number_sets = 64
 number_ways = 4
@@ -14,72 +15,76 @@ number_ways = 4
 DEBUG_MODE = True
 
 def main():
-        if not DEBUG_MODE:
-            if len(sys.argv) != 5:
-                raise Exception("give inputs like python3 vcd_analyze.py import_signals inst.txt xx.vcd trace-dump.json")
+    if not DEBUG_MODE:
+        if len(sys.argv) != 5:
+            raise Exception("give inputs like python3 vcd_analyze.py import_signals inst.txt xx.vcd trace-dump.pickle")
 
-        inst_stat_list = []
-        if not DEBUG_MODE:
-            signal_list = verilog_vcd.import_signals(sys.argv[1])
-            insts, seed = verilog_vcd.parse_commit_log(sys.argv[2])
-            vcd_json = verilog_vcd.parse_vcd(sys.argv[3], siglist=signal_list)
-        else:
-            signal_list = verilog_vcd.import_signals("/home/wyanzhao/Workspace/python-vcd/example/boom_signals")
-            insts, seed = verilog_vcd.parse_commit_log("/home/wyanzhao/Workspace/boom-template/verisim/void.txt")
-            try:
-                with open("void.vcd", "r") as f:
-                    vcd_json = json.load(f)
-            except FileNotFoundError:
-                vcd_json = verilog_vcd.parse_vcd('/home/wyanzhao/Workspace/boom-template/verisim/void.vcd', siglist=signal_list)
-                with open("void.vcd", "w") as f:
-                    json.dump(vcd_json ,f)
+    inst_stat_list = []
+    if not DEBUG_MODE:
+        signal_list = verilog_vcd.import_signals(sys.argv[1])
+        insts, seed = verilog_vcd.parse_commit_log(sys.argv[2])
+        vcd_obj = verilog_vcd.parse_vcd(sys.argv[3], siglist=signal_list)
+    else:
+        signal_list = verilog_vcd.import_signals("/home/wyanzhao/Workspace/python-vcd/example/boom_signals.json")
+        insts, seed = verilog_vcd.parse_commit_log("/home/wyanzhao/Workspace/boom-template/verisim/void-inst.txt")
+        try:
+            with open("void-vcd.pickle", "rb") as f:
+                vcd_obj = pickle.load(f)
+        except FileNotFoundError:
+            vcd_obj = verilog_vcd.parse_vcd('/home/wyanzhao/Workspace/boom-template/verisim/void.vcd', siglist=signal_list)
+            with open("void-vcd.pickle", "wb") as f:
+                pickle.dump(vcd_obj ,f)
 
-        if not DEBUG_MODE:
+    if not DEBUG_MODE:
+        for inst in insts:
+            tick, tsc = verilog_vcd.search_time(inst.get("count"), vcd_obj)
+            signal_stat = verilog_vcd.search_stat(tick, signal_list, vcd_obj)
+            inst_stat_list.append((int(tsc, 2), signal_stat))
+    else:
+        try:
+            with open("inst_stat_list.pickle", "rb") as f:
+                inst_stat_list = pickle.load(f)
+        except FileNotFoundError:
             for inst in insts:
-                tick, tsc = verilog_vcd.search_time(inst.get("count"), vcd_json)
-                signal_stat = verilog_vcd.search_stat(tick, signal_list, vcd_json)
-                inst_stat_list.append((int(tsc, 2), signal_stat))
-        else:
-            try:
-                with open("inst_stat_list.json", "r") as f:
-                    inst_stat_list = json.load(f)
-            except FileNotFoundError:
-                for inst in insts:
-                    tick, tsc = verilog_vcd.search_time(inst.get("count"), vcd_json)
-                    signal_stat = verilog_vcd.search_stat(tick, signal_list, vcd_json)
-                    inst_stat_list.append((int(tsc, 2), signal_stat))
-                    with open("inst_stat_list.json", "w") as f:
-                        json.dump(inst_stat_list, f)
+                tick, tsc = verilog_vcd.search_time(inst.get("count"), vcd_obj)
+                signal_stat = verilog_vcd.search_stat(tick, signal_list, vcd_obj)
+                inst_stat_list.append((int(tsc, 2), signal_stat, int(inst.get('addr'))))
+            with open("inst_stat_list.pickle", "wb") as f:
+                pickle.dump(inst_stat_list, f)
         
+    cache_statuses = []
+    for i in inst_stat_list:
+            cache_status = {}
+            tsc = i[0]
+            inst_addr = i[2]
+            for j in i[1]:
+                if j != "TOP.TestHarness.dut.tile.dcache.s1_load_retry" and  j != "TOP.TestHarness.dut.tile.dcache.s1_dc_bypass" and  j != "TOP.TestHarness.dut.tile.core.debug_tsc_reg[63:0]":
+                    _, _, _, cache_set, cache_way, other, *_ = j.split("_")
+                    if cache_set not in cache_status:
+                        cache_status[cache_set] = {}
+                    if cache_way not in cache_status[cache_set]:
+                        cache_status[cache_set][cache_way] = {}
+                    cache_status[cache_set][cache_way][other] = i[1].get(j)
+                elif j == "TOP.TestHarness.dut.tile.dcache.s1_load_retry":
+                    cache_status["s1_load_retry"] = i[1].get(j)
+                elif j == "TOP.TestHarness.dut.tile.dcache.s1_dc_bypass":
+                    cache_status["s1_dc_bypass"] = i[1].get(j)
+                else:
+                    continue
 
-        
-        cache_statuses = []
-        for i in inst_stat_list:
-                cache_status = {}
-                tsc = i[0]
-                for j in i[1]:
-                    if j != "TOP.TestHarness.dut.tile.dcache.s1_load_retry" and  j != "TOP.TestHarness.dut.tile.dcache.s1_dc_bypass" and j != "TOP.TestHarness.dut.tile.core.debug_tsc_reg[63:0]":
-                        _, _, _, cache_set, cache_way, *_ = j.split("_")
-                        if cache_set not in cache_status:
-                            cache_status[cache_set] = {}
-                        cache_status[cache_set][cache_way] = i[1].get(j)
-                    else:
-                        cache_status[j] = i[1].get(j)
+            cache_statuses.append((tsc, cache_status, inst_addr))
 
-                cache_statuses.append((tsc, cache_status))
+    if len(inst_stat_list) != len(cache_statuses):
+            raise Exception("trace_status != cache_statuses, len prior:{}, len after{}"\
+                .format(len(inst_stat_list), len(cache_statuses)))
 
-        if len(inst_stat_list) != len(cache_statuses):
-                raise Exception("trace_status != cache_statuses, len prior:{}, len aftr{}"\
-                        .format(len(inst_stat_list), len(cache_statuses)))
-
-        if not DEBUG_MODE:
-            with open(sys.argv[4], "w") as f:
-                json.dump(cache_statuses, f)
-                print("Dump trace status succeed")
-        else:
-            with open("dump-vcd.json", "w") as f:
-                json.dump(cache_statuses, f)
-                print("Dump trace status succeed")
+    if not DEBUG_MODE:
+        with open(sys.argv[4], "wb") as f:
+            pickle.dump(cache_statuses, f)
+    else:
+        with open("dump-vcd.pickle", "wb") as f:
+            pickle.dump(cache_statuses, f)
+    print("Dump trace status succeed")
 
 if __name__ == "__main__":
         main()
