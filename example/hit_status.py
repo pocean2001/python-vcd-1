aee551ad107f26ff5399c2b7d6f6d1f417889aa7
Yanzhao Wang <wyanzhao1996@gmail.com>
2019-07-08 15:38:17 -0700

    1. updated analyze and dump hit status functions

--- example/hit_status.py
+++ example/hit_status.py
@@ -15,60 +15,84 @@ import pickle
 number_sets = 64
 number_ways = 4
 
-DEBUG_MODE = True
+DEBUG_MODE = False
 
 def find_cache_hit(meta_idx, tag, cache_trace_status, is_boom_orignal: bool):
     hit_status = []
-    if is_boom_orignal:
+    tag_hash = hash(tag)
 
-        #TODO: Implementate boom origin cache dump function.
-
-        return
-    else:
-        for i in cache_trace_status:
-            tsc = i[0]
-            cache_status = i[1]
-            commit_addr = i[2]
-            hit_set = cache_status[str(meta_idx)]
-            s1_dc_bypass = True if cache_status.get('s1_dc_bypass') == '1' else False
-            s1_load_retry = True if cache_status.get('s1_load_retry') == '1' else False
-            for way in hit_set:
-                s_valid = True if hit_set.get(way).get('s') == '1' else False
-                if tag == hit_set.get(way).get('tag[19:0]') and (not s_valid or (s1_dc_bypass and s1_load_retry)):
-                    hit_status.append("commit inst: {} tsc:{} hit set:{} way:{}".format(hex(commit_addr), tsc, meta_idx, way))
-        return hit_status
+    for i in cache_trace_status:
+        (tsc, cache_status, commit_addr, s1_load_retry, s1_dc_bypass) = i
+        hit_set = cache_status[meta_idx]
+        for way in range(number_ways):
+            if is_boom_orignal:   
+                if tag_hash == hash(hit_set[way].get('tag[19:0]')):
+                    hit_status.append("commit inst: {} tsc:{} hit set:{} way:{}".format(hex(commit_addr), hex(tsc), meta_idx, (way)))
+            else:
+                s_valid = True if hit_set[way].get('s') == '1' else False
+                if tag_hash == hash(hit_set[way].get('tag[19:0]')) and (not s_valid or (s1_dc_bypass and s1_load_retry)):
+                    hit_status.append("commit inst: {} tsc:{} hit set:{} way:{}".format(hex(commit_addr), hex(tsc), (meta_idx), (way)))
+                
+    return hit_status
 
 def main():
     cache_trace = {}
+    using_boom_raw = False
+
     if not DEBUG_MODE:
-        if len(sys.argv) != 3:
-            raise Exception("give inputs like python3 hit_status.py 0x800000 trace-dump.pickle")
+        if len(sys.argv) != 5:
+            raise Exception("give inputs like python3 hit_status.py 80002c60 trace-input.pickle true cache-output.txt")
+
+        print("input inst addr checked: {}".format(sys.argv[1]))
+        print("input dump file: {}".format(sys.argv[2]))
+        if sys.argv[3] == 'True':
+            using_boom_raw = True
+        else:
+            using_boom_raw = False
+
+        print("input if is_boom_orignal: {}".format(using_boom_raw))
+        print("output trace dump file: {}".format(sys.argv[4]))
     
         with open(sys.argv[2], "rb") as f:
             cache_trace = pickle.load(f)
+            f.close()
+
     else:
         with open("/home/wyanzhao/Workspace/python-vcd/dump-vcd.pickle", "rb") as f:
             cache_trace = pickle.load(f)
+            f.close()
 
     if not DEBUG_MODE:
         inst_addr = sys.argv[1]
     else:
-        inst_addr = "80002678"
+        inst_addr = "80002c60"
+    #TODO: inst_addr
+
     inst_addr = int(inst_addr, 16)
+    print("inst addr:{}".format(hex(inst_addr)))
     
     if inst_addr.bit_length() != 32:
-       raise Exception("Length of inst addr is wrong, needs 32 bit, you gave{}".format(inst_addr.bit_length()))
+       raise Exception("Length of inst addr is wrong, needs 32 bit, you gave {}".format(inst_addr.bit_length()))
     inst_bin = bitarray.bitarray(bin(inst_addr).lstrip("-0b"))
     meta_idx = inst_bin[-12:-6]
     tag = bin(inst_addr >> 12).lstrip("-0b")
 
     meta_idx = int(meta_idx.to01(), 2)
 
-    hit_status = find_cache_hit(meta_idx, tag, cache_trace, False)
-    with open("hit_status.txt", "w") as f:
-        for i in hit_status:
-            f.write(i + "'\n")
+    if not DEBUG_MODE:
+        hit_status = find_cache_hit(meta_idx, tag, cache_trace, using_boom_raw)
+        with open(sys.argv[4], "w") as f:
+            for i in hit_status:
+                f.write(i + "\n")
+            f.close()
+    else:
+        hit_status = find_cache_hit(meta_idx, tag, cache_trace, False)
+        with open("void_cache-out.txt", "w") as f:
+            for i in hit_status:
+                f.write(i + "\n")
+            f.close()
 
+    print("cache hit status dump succeed")
     return 0
 
 if __name__ == "__main__":
--- example/vcd_analyze.py
+++ example/vcd_analyze.py
@@ -5,86 +5,114 @@ import sys
 sys.path.append("..")
 sys.path.append(".")
 
-import verilog_vcd
 import json
 import pickle
+import gc
+
+import verilog_vcd
 
 number_sets = 64
 number_ways = 4
 
-DEBUG_MODE = True
+DEBUG_MODE = False
 
 def main():
     if not DEBUG_MODE:
         if len(sys.argv) != 5:
             raise Exception("give inputs like python3 vcd_analyze.py import_signals inst.txt xx.vcd trace-dump.pickle")
 
-    inst_stat_list = []
     if not DEBUG_MODE:
+        print("signal_list:{}".format(sys.argv[1]))
+        print("commit sequences file:{}".format(sys.argv[2]))
+        print("vcd file:{}".format(sys.argv[3]))
+        print("dump file:{}".format(sys.argv[4]))
         signal_list = verilog_vcd.import_signals(sys.argv[1])
         insts, seed = verilog_vcd.parse_commit_log(sys.argv[2])
         vcd_obj = verilog_vcd.parse_vcd(sys.argv[3], siglist=signal_list)
     else:
         signal_list = verilog_vcd.import_signals("/home/wyanzhao/Workspace/python-vcd/example/boom_signals.json")
-        insts, seed = verilog_vcd.parse_commit_log("/home/wyanzhao/Workspace/boom-template/verisim/void-inst.txt")
+        insts, seed = verilog_vcd.parse_commit_log("/home/wyanzhao/Workspace/boom-template/verisim/void.txt")
         try:
             with open("void-vcd.pickle", "rb") as f:
                 vcd_obj = pickle.load(f)
+                f.close()
         except FileNotFoundError:
             vcd_obj = verilog_vcd.parse_vcd('/home/wyanzhao/Workspace/boom-template/verisim/void.vcd', siglist=signal_list)
             with open("void-vcd.pickle", "wb") as f:
                 pickle.dump(vcd_obj ,f)
+                f.close()
 
+    print("Import files and parse vcd finished")
+    gc.collect()
+
+    inst_stat_list = []
     if not DEBUG_MODE:
         for inst in insts:
-            tick, tsc = verilog_vcd.search_time(inst.get("count"), vcd_obj)
-            signal_stat = verilog_vcd.search_stat(tick, signal_list, vcd_obj)
-            inst_stat_list.append((int(tsc, 2), signal_stat))
+                tick, tsc = verilog_vcd.search_time(inst.get("count"), vcd_obj)
+                signal_stat = verilog_vcd.search_stat(tick, signal_list, vcd_obj)
+                inst_stat_list.append((int(tsc, 2), signal_stat, int(inst.get('addr'))))
     else:
         try:
-            with open("inst_stat_list.pickle", "rb") as f:
+            with open("void_stat_list.pickle", "rb") as f:
                 inst_stat_list = pickle.load(f)
+                f.close()
         except FileNotFoundError:
             for inst in insts:
                 tick, tsc = verilog_vcd.search_time(inst.get("count"), vcd_obj)
                 signal_stat = verilog_vcd.search_stat(tick, signal_list, vcd_obj)
                 inst_stat_list.append((int(tsc, 2), signal_stat, int(inst.get('addr'))))
-            with open("inst_stat_list.pickle", "wb") as f:
+            with open("void_stat_list.pickle", "wb") as f:
                 pickle.dump(inst_stat_list, f)
-        
-    cache_statuses = []
+                f.close()
+    
+    print("parse signals status finished")
+    del signal_list, insts, vcd_obj
+    gc.collect()
+
+    cache_trace = []
+    s1_load_retry_hash = hash("TOP.TestHarness.dut.tile.dcache.s1_load_retry")
+    s1_dc_bypass_hash = hash("TOP.TestHarness.dut.tile.dcache.s1_dc_bypass")
+    tsc_hash = hash("TOP.TestHarness.dut.tile.core.debug_tsc_reg[63:0]")
+
     for i in inst_stat_list:
-            cache_status = {}
-            tsc = i[0]
-            inst_addr = i[2]
-            for j in i[1]:
-                if j != "TOP.TestHarness.dut.tile.dcache.s1_load_retry" and  j != "TOP.TestHarness.dut.tile.dcache.s1_dc_bypass" and  j != "TOP.TestHarness.dut.tile.core.debug_tsc_reg[63:0]":
-                    _, _, _, cache_set, cache_way, other, *_ = j.split("_")
-                    if cache_set not in cache_status:
-                        cache_status[cache_set] = {}
-                    if cache_way not in cache_status[cache_set]:
-                        cache_status[cache_set][cache_way] = {}
-                    cache_status[cache_set][cache_way][other] = i[1].get(j)
-                elif j == "TOP.TestHarness.dut.tile.dcache.s1_load_retry":
-                    cache_status["s1_load_retry"] = i[1].get(j)
-                elif j == "TOP.TestHarness.dut.tile.dcache.s1_dc_bypass":
-                    cache_status["s1_dc_bypass"] = i[1].get(j)
+            tsc, signal_stat, inst_addr = i
+            cache = [[{} for _ in range(number_ways)] for _ in range(number_sets)]
+            
+            s1_load_retry = None
+            s1_dc_bypass = None
+
+            for signal_name in signal_stat:
+                j_hash = hash(signal_name)
+                if j_hash == s1_load_retry_hash:
+                    s1_load_retry = bool(signal_stat.get(signal_name))
+                elif j_hash == s1_dc_bypass_hash:
+                    s1_dc_bypass = bool(signal_stat.get(signal_name))
+                elif j_hash != tsc_hash:
+                    _, _, _, cache_set, cache_way, other, *_ = signal_name.split("_")
+                    cache_set = int(cache_set)
+                    cache_way = int(cache_way)
+                    cache[cache_set][cache_way][other] = signal_stat.get(signal_name)
                 else:
                     continue
+            cache_trace.append((tsc, cache, inst_addr, s1_load_retry, s1_dc_bypass))
 
-            cache_statuses.append((tsc, cache_status, inst_addr))
-
-    if len(inst_stat_list) != len(cache_statuses):
+    if len(inst_stat_list) != len(cache_trace):
             raise Exception("trace_status != cache_statuses, len prior:{}, len after{}"\
-                .format(len(inst_stat_list), len(cache_statuses)))
+                .format(len(inst_stat_list), len(cache_trace)))
+
+    print("parse cache status finished")
+    del inst_stat_list
+    gc.collect()
 
     if not DEBUG_MODE:
         with open(sys.argv[4], "wb") as f:
-            pickle.dump(cache_statuses, f)
+            pickle.dump(cache_trace, f)
+            f.close()
     else:
         with open("dump-vcd.pickle", "wb") as f:
-            pickle.dump(cache_statuses, f)
-    print("Dump trace status succeed")
+            pickle.dump(cache_trace, f)
+            f.close()
+    print("dump trace status succeed")
 
 if __name__ == "__main__":
         main()
